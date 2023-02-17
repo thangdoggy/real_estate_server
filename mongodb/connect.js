91a3872f11fdfb85fa2b75fff4abeefb95f8c05a
thangdoggy <hhqthang@gmail.com>
2023-02-18 00:09:51 +0700

    back-end setup

---
+++ mongodb/connect.js
@@ -0,0 +1,14 @@
+import mongoose, { Error, mongo } from 'mongoose';
+
+const connectDB = (url) => {
+  mongoose.set('strictQuery', true);
+
+  mongoose
+    .connect(url)
+    .then(() => console.log('MongoDB connected'))
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+export default connectDB;
