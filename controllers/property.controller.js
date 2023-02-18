6f6023268ac41ad4bd2a1f2f4e462c70c3630828
thangdoggy <hhqthang@gmail.com>
2023-02-18 21:40:14 +0700

    routes and controllers setup

---
+++ controllers/property.controller.js
@@ -0,0 +1,16 @@
+import Property from '../mongodb/models/property.js';
+import User from '../mongodb/models/user.js';
+
+const getAllProperties = async (req, res) => {};
+const getPropertyDetails = async (req, res) => {};
+const createProperty = async (req, res) => {};
+const updateProperty = async (req, res) => {};
+const deleteProperty = async (req, res) => {};
+
+export {
+  getAllProperties,
+  getPropertyDetails,
+  createProperty,
+  updateProperty,
+  deleteProperty,
+};
